04bec3f6eca982013803e18f44675e1a947d7e5d
haf-abe <haftuabebe52@gmail.com>
2022-06-10 20:55:39 +0100

    Updates

---
+++ precache-manifest.55939598d863c478276aca2768119dcc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c82eac2abf800075c505170a8ef929e",
+    "url": "/my-social-site/index.html"
+  },
+  {
+    "revision": "06a43bb9b1fdcec2aecc",
+    "url": "/my-social-site/static/css/2.7c5c0350.chunk.css"
+  },
+  {
+    "revision": "b14239efae41fd64f17d",
+    "url": "/my-social-site/static/css/main.8785ace8.chunk.css"
+  },
+  {
+    "revision": "06a43bb9b1fdcec2aecc",
+    "url": "/my-social-site/static/js/2.8c07c4f6.chunk.js"
+  },
+  {
+    "revision": "2cb479a22703689952d91fd12ba22fbf",
+    "url": "/my-social-site/static/js/2.8c07c4f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b14239efae41fd64f17d",
+    "url": "/my-social-site/static/js/main.6061cc47.chunk.js"
+  },
+  {
+    "revision": "840bcc6cec298263ddc9",
+    "url": "/my-social-site/static/js/runtime-main.59fecd6c.js"
+  },
+  {
+    "revision": "630f7fda85f7b35cb42cbe1be64ec8f0",
+    "url": "/my-social-site/static/media/FaceTube.630f7fda.png"
+  },
+  {
+    "revision": "931a98bc05e592b9385c9e93315f65b7",
+    "url": "/my-social-site/static/media/dislike.931a98bc.png"
+  },
+  {
+    "revision": "d1fd0755f74a9609c0333dd29daebdb2",
+    "url": "/my-social-site/static/media/like.d1fd0755.png"
+  }
+]);
